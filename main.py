89441589b0f74095847593d0763f6a373d0f97d3
SungKyu Lee, (Kai Lee) <lkai04318@gmail.com>
2024-11-01 16:17:57 +0900

    Fixed commit error, Added face/pose/hand landmarks

--- main.py
+++ main.py
@@ -12,23 +12,32 @@ mediapipe_drawing = mediapipe.solutions.drawing_utils   # MediaPipe Drawing Util
 
 def mediapipe_detection(image, model):                  # Image = Frame
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)      # Convert BGR -> RGB
-    image.flags.writable = False
+    image.flags.writeable = False
     results = model.process(image)                      # MediaPipe Model Prediction
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)      # Convert back to RGB -> BGR
     return image, results
 
+def mediapipe_detection_draw_landmarks(image, results):
+    mediapipe_drawing.draw_landmarks(image, results.face_landmarks, mediapipe_holistic.FACE_CONNECTIONS)
+    mediapipe_drawing.draw_landmarks(image, results.pose_landmarks, mediapipe_holistic.POSE_CONNECTIONS)
+    mediapipe_drawing.draw_landmarks(image, results.left_hand_landmarks, mediapipe_holistic.HAND_CONNECTIONS)
+    mediapipe_drawing.draw_landmarks(image, results.right_hand_landmarks, mediapipe_holistic.HAND_CONNECTIONS)
+
+
 if __name__ == '__main__':
-    cap = cv2.VideoCapture(0)                     # Accessing Webcam
-    with mediapipe_holistic.Holistic(min_detection_confidence= 0.5, min_tracking_confidence = 0.5) as holistic:     # Access MediaPipe Model
+    cap = cv2.VideoCapture(0)                                           # Accessing Webcam
+    with mediapipe_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:     # Access MediaPipe Model
         while cap.isOpened():
-            ret, frame = cap.read()                   # Read Feed
+            ret, frame = cap.read()                                     # Read Feed
 
-            #image, results = mediapipe_detection(frame, model)  # Start Detection
+            image, results = mediapipe_detection(frame, holistic)       # Start Detection
 
-            cv2.imshow('Input Sign Language', frame)  # Show Feed
+            mediapipe_detection_draw_landmarks(image, results)          # Draw Landmarks
 
-            if cv2.waitKey(10) & 0xFF == ord('q'):    # End Feed
+            cv2.imshow('Input Sign Language', image)                    # Show Feed
+
+            if cv2.waitKey(10) & 0xFF == ord('q'):                      # End Feed
                 break
         cap.release()
         cv2.destroyAllWindows()
