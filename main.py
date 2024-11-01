cd26fb0f77850b573ab2f9487ed37b5216da0988
SungKyuLee <cnusungkyu@o.cnu.ac.kr>
2024-11-01 16:51:25 +0900

    Draw Landmark complete for debugging, checked if mediapipe is recieving landmarks each frame

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from matplotlib import pyplot as plt
 import time
 import mediapipe as mediapipe
 
+
 mediapipe_holistic = mediapipe.solutions.holistic       # MediaPipe Holistic Model (Detecting)
 mediapipe_drawing = mediapipe.solutions.drawing_utils   # MediaPipe Drawing Utilities (Drawing)
 
@@ -18,12 +19,22 @@ def mediapipe_detection(image, model):                  # Image = Frame
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)      # Convert back to RGB -> BGR
     return image, results
 
-def mediapipe_detection_draw_landmarks(image, results):
-    mediapipe_drawing.draw_landmarks(image, results.face_landmarks, mediapipe_holistic.FACE_CONNECTIONS)
-    mediapipe_drawing.draw_landmarks(image, results.pose_landmarks, mediapipe_holistic.POSE_CONNECTIONS)
-    mediapipe_drawing.draw_landmarks(image, results.left_hand_landmarks, mediapipe_holistic.HAND_CONNECTIONS)
-    mediapipe_drawing.draw_landmarks(image, results.right_hand_landmarks, mediapipe_holistic.HAND_CONNECTIONS)
+def mediapipe_detection_draw_landmarks(image, results):     # For Debugging
+    mediapipe_drawing.draw_landmarks(image, results.face_landmarks, mediapipe_holistic.FACEMESH_TESSELATION,
+                                     mediapipe_drawing.DrawingSpec(color=(255,0,0), thickness=1, circle_radius=1),  # Dot Color/Size
+                                     mediapipe_drawing.DrawingSpec(color=(0,255,0), thickness=1, circle_radius=1))  # Line Color/Size
 
+    mediapipe_drawing.draw_landmarks(image, results.pose_landmarks, mediapipe_holistic.POSE_CONNECTIONS,
+                                     mediapipe_drawing.DrawingSpec(color=(255, 0, 0), thickness=2, circle_radius=3), # Dot Color/Size
+                                     mediapipe_drawing.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2)) # Line Color/Size
+
+    mediapipe_drawing.draw_landmarks(image, results.left_hand_landmarks, mediapipe_holistic.HAND_CONNECTIONS,
+                                     mediapipe_drawing.DrawingSpec(color=(255, 0, 0), thickness=2, circle_radius=3), # Dot Color/Size
+                                     mediapipe_drawing.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2)) # Line Color/Size
+
+    mediapipe_drawing.draw_landmarks(image, results.right_hand_landmarks, mediapipe_holistic.HAND_CONNECTIONS,
+                                     mediapipe_drawing.DrawingSpec(color=(255, 0, 0), thickness=2, circle_radius=3), # Dot Color/Size
+                                     mediapipe_drawing.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2)) # Line Color/Size
 
 if __name__ == '__main__':
     cap = cv2.VideoCapture(0)                                           # Accessing Webcam
